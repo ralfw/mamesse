bc5f46443334f471bf571076f4709d05a3a72975
Ralf Westphal <info@ralfw.de>
2014-02-10 15:16:33 +0100

    App und Build Solution

---
+++ source/mam.steuerung.application/mam.steuerung.application/App.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace mam.steuerung.application
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            var ui = new ui.UI();
+            var dom = new domäne.Domäne();
+            var server = new serverportal.ServerPortal();
+            var servicedesk = new clientprovider.ClientProvider();
+
+            ui.Hilferuf += dom.HilfeAnfordern;
+            dom.HilfestatusGeändert += ui.HilfestatusAnzeigen;
+            dom.FunktionsstatusGeändert += ui.FunktionsstatusAnzeigen;
+
+            dom.Hilferuf += servicedesk.HilfeAnfordern;
+
+            server.Zurücksetzungswunsch += dom.Zurücksetzen;
+            server.Fehler += dom.Kaputtgehen;
+            server.Reparaturwunsch += dom.Reparieren;
+            server.HilfeUnterwegs += dom.Beruhigen;
+
+            using(server)
+            using(servicedesk)
+            {
+                ui.Starten();
+            }
+        }
+    }
+}
